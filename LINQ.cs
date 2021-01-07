c22b3805e3d6282bba654b85a62f59694b9b3f13
Iryna <ortofosfatnyi@gmail.com>
2021-01-07 16:27:09 +0200

    Making list int and filter selection

--- LINQ.cs
+++ LINQ.cs
@@ -8,9 +8,19 @@ namespace Homework14
 {
     public class LINQ
     {
-        public void Country()
+        
+        public void MetodLinq()
         {
+            var collection = new List<int>();
 
+            for (var i = 0; i < 10; i++)
+            {
+                collection.Add(1);
+            }
+
+            var result = from item in collection
+                         where item < 5
+                         select item;
         }
     }
 }
