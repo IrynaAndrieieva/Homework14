4d905bd51e7e0962069595f9f8c09a9f66a22c0e
Iryna <ortofosfatnyi@gmail.com>
2021-01-07 17:08:18 +0200

    Added class Product and made a list with it

--- LINQ.cs
+++ LINQ.cs
@@ -8,22 +8,41 @@ namespace Homework14
 {
     public class LINQ
     {
-        
-        public void MetodLinq()
-        {
-            var collection = new List<int>();
+        Random rnd = new Random();
 
-            for (var i = 0; i < 10; i++)
+        public void MetodLinq()
+        {            
+            var collection = new List<Product>();
+
+            for (var i = 0; i < 100; i++)
             {
-                collection.Add(i);
+                var product = new Product()
+                {
+                    Name = "Product" + i,
+                    Energy = rnd.Next(0, 500)
+                };
+
+                collection.Add(product);
             }
 
             var result = from item in collection
-                         where item < 5
+                         where item.Energy < 200
                          select item;
+
+            var result2 = collection.Where(item => item.Energy < 200)
+                                    .OrderBy(item => item.Energy);
 
             foreach (var item in result)
             {
+                
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("__________");
+
+            foreach (var item in result2)
+            {
+                
                 Console.WriteLine(item);
             }
         }
