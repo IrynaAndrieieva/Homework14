3d4844d26a1b2ad89066762adc71d51c1d1cb9b7
Iryna <ortofosfatnyi@gmail.com>
2021-01-10 15:27:33 +0200

    Added logger base

---
+++ Logger/FileService.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework14
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    
+    public class FileService
+    {       
+        public static void WriteLine(string typeLog, string message)
+        {
+            string dirpath = @"E:\Log";
+            DirectoryInfo dirInfo = new DirectoryInfo(dirpath);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            string writePath = @"E:\Log\Log.txt";
+
+            // создание тхт файла
+            using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(string.Format("{0}: {1}: {2}", DateTime.Now.ToLongTimeString(), typeLog, message));
+            }
+        }
+    }
+}
---
+++ Logger/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework14
+{
+    class Logger
+    {
+        private readonly Logger logger = Logger.Instance;
+        private static readonly Logger Instance;
+
+        public void LoggerInfo()
+        {
+            this.logger.LogMessage("Info", "Start method");
+            FileService.WriteLine("Info", "Start method");
+        }
+
+        public void LoggerError()
+        {
+            this.logger.LogMessage("Error", "Action failed by reason");
+            FileService.WriteLine("Error", "Action failed by reason");
+
+            try
+            {
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"I broke a toilet");
+            }
+        }
+
+        internal void LogMessage(string typeLog, string message)
+        {
+            Console.WriteLine("[{0}]: {1}: {2}", DateTime.Now.ToLongTimeString(), typeLog, message);
+        }
+    }
+}
