c22ae98c4bcaa0c48bd6b917f8f18f1f41fd2917
Iryna <ortofosfatnyi@gmail.com>
2021-01-11 15:01:34 +0200

    Trying make Async from Logger messege and file servis

--- Logger/Actions.cs
+++ Logger/Actions.cs
@@ -10,16 +10,43 @@ namespace Homework14.Logger
     {      
         private readonly Loger loger = Loger.Instance;
 
-        public void LoggerInfo()
+        #region tryingMakeInfoAsync
+
+        public void LoggerInfoConslAndFile()
         {
-            this.loger.LogMessage("Info", "Start method");
+            LoggerInfoAsync();
             FileService.WriteLine("Info", "Start method");
         }
-    
-        public void LoggerError()
+        public async Task LoggerInfoAsync()
+        {
+            await Task.Run(() => LoggerInfoFromConsl());
+        }
+
+        public void LoggerInfoFromConsl()
+        {
+            this.loger.LogMessage("Info", "Start method");           
+        }
+
+        #endregion
+
+      
+        #region tryingMakeErrorAsync
+
+        public void LoggerErrorConslAndFile()
+        {
+            LoggerErrorAsync();
+            FileService.WriteLine("Error", "Action failed by reason");
+        }
+        public async Task LoggerErrorAsync()
+        {
+            await Task.Run(() => LoggerErrorromConsl());
+        }
+
+        public void LoggerErrorromConsl()
         {
             this.loger.LogMessage("Error", "Action failed by reason");
-            FileService.WriteLine("Error", "Action failed by reason");         
         }
+
+        #endregion
     }
 }
