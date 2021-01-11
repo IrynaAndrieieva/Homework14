0b0b6003f99bdc00bdd3bf56e7c5f4e02ac7c424
Iryna <ortofosfatnyi@gmail.com>
2021-01-11 13:13:00 +0200

    Trying to call an event after 10 ID (dont worker)

--- Logger/FileService.cs
+++ Logger/FileService.cs
@@ -1,5 +1,6 @@
 ﻿namespace Homework14
 {
+    using Homework14.Logger;
     using System;
     using System.IO;
     using System.Reflection;
@@ -9,38 +10,35 @@
     {
         public delegate void FileEntries(string message);
 
-        public event FileEntries Notify;
-
-        public static int ID { get; set; }
+        public static event FileEntries Notify;       
 
         public static void WriteLine(string typeLog, string message)
         {
-
+            
             string dirpath = @"D:\Log";
             DirectoryInfo dirInfo = new DirectoryInfo(dirpath);
 
             if (!dirInfo.Exists)
             {
                 dirInfo.Create();
-            }
+            }           
 
             string writePath = @"D:\Log\Log.txt";
 
-            // создание тхт файла
-            using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+            int Id = 0;
+
+            for (Id = 0; Id < 11; Id++)
             {
-                sw.WriteLine(string.Format("{0}: {1}: {2}", DateTime.Now.ToLongTimeString(), typeLog, message) + ID);
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(string.Format("{0}: {1}: {2}", DateTime.Now.ToLongTimeString(), typeLog, message) + Id);
+                }
+            }
+            if (Id == 10)
+            {
+                Notify?.Invoke($"At the moment in the file {Id}");   // 2.Вызов события
             }
 
-
-        }
-
-        public void Take()
-        {
-            if (ID == 10)
-            {
-                Notify?.Invoke($"At the moment in the file {ID}");   // 2.Вызов события
-            }
-        }
+        }       
     }
 }
